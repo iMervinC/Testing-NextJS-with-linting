fa115e67f4c518c3886cddc572cedd921eed2856
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-09-07 13:38:14 +0800

    added code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,21 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./src/jest.setup.ts'],
-
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
   globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.test.json',
+    "ts-jest": {
+      tsconfig: "./tsconfig.test.json",
     },
   },
-}
+  jest: {
+    coverageThreshold: {
+      global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
+  },
+};
