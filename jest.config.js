b2347aa24dc798c031e780331ba07d7438c9da63
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-09-07 12:19:25 +0800

    Config with React-testing-library and jest-dom

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./src/jest.setup.ts'],
+
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.test.json',
+    },
+  },
+}
---
+++ src/components/__test__/Hello.test.tsx
@@ -0,0 +1,9 @@
+import { screen, render } from '@testing-library/react'
+import Hello from '../Hello'
+
+describe('hello components', () => {
+  it('renders', () => {
+    render(<Hello />)
+    expect(screen.getByText(/hello/i)).toBeInTheDocument()
+  })
+})
